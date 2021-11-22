803bd49fc4be367140e70a8f06783b1d7a2ed1c4
Schubert-Patrick <73674234+Schubert-Patrick@users.noreply.github.com>
2021-11-22 03:56:18 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,49 @@
+<?php echo`
+<nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                Patrick Schubert
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="#title" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about-me" class="nav-link">About Me</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link services" data-bs-toggle="dropdown" id="navbarDropdown">Services
+                            <i class="fa fa-caret-down"></i></a>
+                            <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" href="#">Search Engine Optimization (SEO)</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#skills" class="nav-link">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#experience" class="nav-link">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#cta" class="nav-link">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#education" class="nav-link">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="hobbies.html" class="nav-link">Hobbies</a>
+                    </li>
+                    <li class="nav-item contact-nav-item">
+                        <a href="#contact" class="nav-link contact-btn-nav">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>`;
+?>
